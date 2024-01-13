3468c2131375d1caf65bc2068dec033f64dfbb40
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-01-13 16:46:34 +0300

    Changes

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = (driversArray) => {
+  return driversArray.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (driversArray) => {
+  return driversArray.slice(-2);
+};
+// slice splice
+function selectingDrivers(driversArray) {
+  return [
+    returnFirstTwoDrivers(driversArray),
+    returnLastTwoDrivers(driversArray),
+  ];
+}
+
+const createFareMultiplier = (value) => {
+  return function () {
+    return value * value;
+  };
+};
+
+const fareDoubler = (fare) => createFareMultiplier(fare)();
+const fareTripler = (fare) => createFareMultiplier(fare)() * fare;
+
+const selectDifferentDrivers = (driversArray, fn) => {
+  return fn(driversArray);
+};
